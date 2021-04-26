8967c59e44f97ea7fa98e0fd1993fc2e1701f1db
ShIRannx <shirann@qq.com>
2021-04-26 11:37:10 +0800

    testRun_8

--- src/org/shirann/firstModule.groovy
+++ src/org/shirann/firstModule.groovy
@@ -6,7 +6,7 @@ def printMes(content){
 
 
 def build(buildType, buildShell){
-  def buildTools = ["mvn": "M2", "gradle": "Gradle", "ant": "Ant", "npm": "Nodejs"]
+  def buildTools = ["maven": "M2", "gradle": "Gradle", "ant": "Ant", "npm": "Nodejs"]
   println("当前选择的构建类型为:${buildType}")
   buildToolHome = tool buildTools[buildType]
   sh "${buildToolHome}/bin/${buildType} ${buildShell}"
